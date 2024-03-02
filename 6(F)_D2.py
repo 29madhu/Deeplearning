87b0cd229a07c147e4d56c3ec1cbfc93dd1627d3
29madhu <129149497+29madhu@users.noreply.github.com>
2024-03-02 12:37:52 +0530

    Add files via upload

---
+++ 6(F)_D2.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+
+
+dataset = pd.read_csv("C:/Users/pothu/OneDrive/Desktop/9e7fdfb8189f0cdf5d73a494e4a6392a-aaecbd14aeaa468cd749528f291aa8a30c2ea09e/iris_dataset.csv")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=39)
+
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+classifier.fit(X_train, y_train)
+
+
+y_pred = classifier.predict(X_test)
+
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print('Accuracy:', accuracy_score(y_test, y_pred))
